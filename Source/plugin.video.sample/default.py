aaadf6b5b99660e8f3f887d14a08e1b815254d14
marduk191 <marduk191@gmail.com>
2015-12-04 23:51:05 -0600

    initial upload

---
+++ Source/plugin.video.sample/default.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------
+# License: GPL (http://www.gnu.org/licenses/gpl-3.0.html)
+# Based on code from youtube addon
+#------------------------------------------------------------
+
+import os
+import sys
+import plugintools
+import xbmc,xbmcaddon
+from addon.common.addon import Addon
+
+addonID = 'plugin.video.maryoutube'
+addon = Addon(addonID, sys.argv)
+local = xbmcaddon.Addon(id=addonID)
+icon = local.getAddonInfo('icon')
+
+YOUTUBE_CHANNEL_ID = "maryoutube"
+
+# Entry point
+def run():
+    plugintools.log(""+YOUTUBE_CHANNEL_ID+".run")
+    
+    # Get params
+    params = plugintools.get_params()
+    
+    if params.get("action") is None:
+        main_list(params)
+    else:
+        action = params.get("action")
+        exec action+"(params)"
+    
+    plugintools.close_item_list()
+
+# Main menu
+def main_list(params):
+    plugintools.log(""+YOUTUBE_CHANNEL_ID+".main_list "+repr(params))
+
+    plugintools.add_item( 
+        #action="", 
+        title="thisisthename",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID+"/",
+        thumbnail=icon,
+        folder=True )
+
+run()
